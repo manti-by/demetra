4b36a43bd1dc2b6cd8fff670210b1166564901a2
Alexander Chaika <manti.by@gmail.com>
2025-11-02 10:00:00 +0300

    Add settings module with environment config

--- chimera/settings.py
+++ chimera/settings.py
@@ -1,5 +1,3 @@
+import os
 PROJECTS_PATH = os.getenv("PROJECTS_PATH", "/projects")
 LINEAR_API_KEY = os.getenv("LINEAR_API_KEY")
-LINEAR_API_URL = "https://api.linear.app/graphql"
-OPENCODE_PATH = os.getenv("OPENCODE_PATH", "/usr/local/bin/opencode")
-CODERABBIT_PATH = os.getenv("CODERABBIT_PATH", "/usr/local/bin/coderabbit")
