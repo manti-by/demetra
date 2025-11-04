ca9d775b468911f7cb6e03c3e956aacaa93bd4a0
Alexander Chaika <manti.by@gmail.com>
2025-11-04 10:00:00 +0300

    Add Linear GraphQL client integration

--- chimera/services/linear.py
+++ chimera/services/linear.py
@@ -1,10 +1,6 @@
-from chimera.settings import LINEAR_API_KEY, LINEAR_API_URL
 import httpx
+from chimera.settings import LINEAR_API_KEY
 
 class LinearClient:
     def __init__(self):
         self.api_key = LINEAR_API_KEY
-        self.url = LINEAR_API_URL
-
-    async def get_issues(self, team_id: str):
-        pass
