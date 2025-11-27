cc0123828be968b3d909a070d88277145307b007
Alexander Chaika <manti.by@gmail.com>
2025-11-27 10:00:00 +0300

    Add settings with Linear API URL

--- chimera/settings.py
+++ chimera/settings.py
@@ -1,3 +1,5 @@
 import os
 PROJECTS_PATH = os.getenv("PROJECTS_PATH", "/projects")
 LINEAR_API_KEY = os.getenv("LINEAR_API_KEY")
+LINEAR_API_URL = "https://api.linear.app/graphql"
+LINEAR_TEAM_ID = os.getenv("LINEAR_TEAM_ID")
