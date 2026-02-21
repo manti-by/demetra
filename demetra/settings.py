cf70a9783b748d20fb570019a3968abe198f0d75
Alexander Chaika <manti.by@gmail.com>
2026-02-21 13:56:51 +0300

    Add awaiting input state id

--- demetra/settings.py
+++ demetra/settings.py
@@ -16,6 +16,9 @@ LINEAR_TEAM_ID = os.environ.get("LINEAR_TEAM_ID")
 LINEAR_STATE_TODO_ID = os.environ.get("LINEAR_STATE_TODO_ID", "9f3c586f-640a-4f78-8170-90217270a0c5")
 LINEAR_STATE_IN_PROGRESS_ID = os.environ.get("LINEAR_STATE_IN_PROGRESS_ID", "ded08079-9ddf-43cb-8aa8-722ba107b691")
 LINEAR_STATE_IN_REVIEW_ID = os.environ.get("LINEAR_STATE_IN_REVIEW_ID", "34829892-5ab6-40a4-af4e-7a73636a78a4")
+LINEAR_STATE_AWAITING_INPUT_ID = os.environ.get(
+    "LINEAR_STATE_AWAITING_INPUT_ID", "e733f22b-fe21-401a-bf68-d2d374507f06"
+)
 
 OPENCODE_PATH = Path(os.environ.get("OPENCODE_PATH", HOME_PATH / ".opencode/bin/opencode"))
 OPENCODE_MODEL = os.environ.get("OPENCODE_MODEL", "opencode/minimax-m2.5-free")
