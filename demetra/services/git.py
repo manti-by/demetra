4675c98fc0d68468cdc9aca9b3ea54e7099fdfa8
Alexander Chaika <manti.by@gmail.com>
2026-03-05 19:17:34 +0300

    MNT-47: Worktree path already exists

--- demetra/services/git.py
+++ demetra/services/git.py
@@ -9,7 +9,7 @@ from demetra.settings import GIT
 async def git_worktree_create(target_path: Path, branch_name: str) -> Path:
     worktree_path = GIT["worktree_path"] / branch_name
     if worktree_path.exists():
-        raise RuntimeError(f"Worktree path already exists: {worktree_path}")
+        await git_worktree_remove(target_path=target_path, worktree_path=worktree_path, force=True)
 
     command = [str(GIT["path"]), "worktree", "add", "-b", branch_name, str(worktree_path)]
     await run_command(command=command, target_path=target_path)
